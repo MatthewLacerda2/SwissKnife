fe25b8b7ea91e278b1d75c5166c462ecea1d71e3
MatthewLacerda2 <math2.0@hotmail.com>
2023-06-15 19:39:11 -0300

    .

--- updateArch.sh
+++ updateArch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "Updating Pacman now"
+echo "*** Updating Pacman now"
 echo ""
 
 sleep 1
@@ -9,7 +9,7 @@ sleep 1
 sudo pacman -Syu
 
 echo ""
-echo "---Updating Yay now---"
+echo "*** Updating Yay now"
 echo ""
 
 sleep 3
@@ -17,7 +17,7 @@ sleep 3
 yay -Syu #dont run as 'sudo' as it can cause perma damage
 
 echo ""
-echo "Updating Flatpak..."
+echo "*** Updating Flatpak now"
 echo ""
 
 sleep 3
@@ -28,5 +28,5 @@ flatpak repair
 echo ""
 echo "-------"
 echo ""
-echo "You're all set. Better not to linger here, alright?"
+echo "You're set. But better not to linger here though, eh?"
 echo ""
