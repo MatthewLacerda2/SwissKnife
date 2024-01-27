8d065172ca6f378745fb5bbed3b5f30e7d6d6332
Lendacerda <math2.0@hotmail.com>
2024-01-26 22:43:36 -0300

    Colors were getting passed down. Fixed (subjective)

--- updateUbuntu.sh
+++ updateUbuntu.sh
@@ -2,7 +2,7 @@
 
 sudo echo ""
 echo -e "\e[35mUpdating..."
-echo ""
+echo -e "\e[36m"
 
 sleep 1
 
@@ -10,7 +10,7 @@ sudo apt-get update
 
 echo ""
 echo -e "\e[35mUpgrading..."
-echo ""
+echo -e "\e[10m"
 
 sleep 2
 
@@ -18,14 +18,14 @@ sudo apt-get upgrade
 
 echo ""
 echo -e "\e[35mUpdating Firmware..."
-echo ""
+echo -e "\e[10m"
 
 sudo fwupdtool get-upgrades
 sudo fwupdtool upgrade
 
 echo ""
 echo -e "\e[35mSnap refreshing..."
-echo ""
+echo -e "\e[10m"
 
 sudo snap refresh
 
@@ -34,6 +34,7 @@ echo -e "\e[35mSnap Refreshed!"
 echo ""
 echo "--------"
 echo ""
-notify-send -i ./arch.png "Deb Update" "Complete!"
+notify-send -i ./arch.png "Debian Update" "Complete!"
 echo -e "\e[32mYou're set. But better not to linger here though, eh?"
 echo ""
+
